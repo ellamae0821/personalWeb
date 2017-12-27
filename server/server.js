795094448e3a4bfb30bd3ca85780a15f2a00a81c
Ella Mae A <ellamae207@gmail.com>
2017-12-26 22:37:00 -1000

    modified 8080 app.use

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ const path            = require('path');
 const PORT            = process.env.PORT || 8080;
 const app             = express();
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.get('/', ( req, res ) =>{
