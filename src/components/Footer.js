9d24aa4ede441a63ff1ffecd8faaab6c6c0150c3
Ella Mae A <ellamae207@gmail.com>
2018-03-14 19:11:32 -1000

    footer sticky bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,16 @@ class Footer extends Component {
   render() {
     return (
       <div className='footer'>
-        <a href="https://github.com/ellamae0821/">
-          <i class="fa fa-github"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/ella-mae-arana/">
-          <i class="fa fa-linkedin"></i>
-        </a>
-        <a href="mailto:ella.d.arana@gmail.com">
-          <i class="fa fa-envelope"></i>
-        </a>
-        <div className="madeBy">
-          Ella Mae Arana © 2017
+        <div>
+          <a href="https://github.com/ellamae0821/">
+            <i class="fa fa-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/ella-mae-arana/">
+            <i class="fa fa-linkedin"></i>
+          </a>
+          <a href="mailto:ella.d.arana@gmail.com">
+            <i class="fa fa-envelope"></i>
+          </a>
         </div>
       </div>
     )
