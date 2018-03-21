3d5e8368c83fe3d92aa8595e3387c7c038897e0f
Ella Mae A <ellamae207@gmail.com>
2018-03-20 21:38:11 -1000

    updated resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -9,7 +9,7 @@ class Resume extends Component {
         <div className='resume'>
 
         </div>
-        <iframe class="scribd_iframe_embed" title="Resume 2017 - Ella Arana (HI)" src="https://www.scribd.com/embeds/372381073/content?start_page=1&view_mode=scroll&access_key=key-3rkdbUD7iQDwTUbFvbU8&show_recommendations=true" data-auto-height="false" data-aspect-ratio="0.7729220222793488" scrolling="no" id="doc_87274" width="100%" height="600" frameborder="0"></iframe>
+        <iframe class="scribd_iframe_embed" title="Resume 2017 - Ella Arana (HI)" src="https://www.scribd.com/embeds/374493609/content?start_page=1&view_mode=scroll&access_key=key-ausm2Q4LhyG7hCxzV6g4&show_recommendations=true" data-auto-height="false" data-aspect-ratio="0.7729220222793488" scrolling="no" id="doc_87274" width="100%" height="600" frameborder="0"></iframe>
       </div>
     )
   }
@@ -17,5 +17,5 @@ class Resume extends Component {
 
 export default Resume;
 
-
+<iframe class="scribd_iframe_embed" title="Web Dev - Ella Mae Arana" src="https://www.scribd.com/embeds/374493609/content?start_page=1&view_mode=scroll&access_key=key-ausm2Q4LhyG7hCxzV6g4&show_recommendations=true" data-auto-height="false" data-aspect-ratio="0.7729220222793488" scrolling="no" id="doc_97139" width="100%" height="600" frameborder="0"></iframe>
 
